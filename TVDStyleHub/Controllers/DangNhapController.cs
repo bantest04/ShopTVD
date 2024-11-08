b33a80cecc449f5e085007884adcc1d200e13701
bantest04 <truongquanghoaivu@gmail.com>
2024-11-08 15:54:53 +0700

    first commit

---
+++ TVDStyleHub/Controllers/DangNhapController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TVDStyleHub.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        // GET: DangNhap
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult DangNhap(string username, string password)
+        {
+            if ("admin".Equals(username) && "admin".Equals(password))
+            {
+                Session["Admin"] = username;
+                return RedirectToAction("DanhMucCacSanPham", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+    }
+}
